2f61c38824e2c7d03a4a3458ee8323b501eeab11
Devin28 <43255667+Devin28@users.noreply.github.com>
2019-09-26 17:26:53 -0700

    Add files via upload

---
+++ BegginerLayout/BegginerLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BegginerLayout
+//
+//  Created by HGPMAC82 on 6/17/19.
+//  Copyright © 2019 HGPMAC82. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
